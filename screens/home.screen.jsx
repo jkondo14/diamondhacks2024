79ea88c795abc33f1fa4487b474e7c4ba5b06228
jkondo14 <146896972+jkondo14@users.noreply.github.com>
2024-04-06 14:28:14 -0700

    Add files via upload

---
+++ screens/home.screen.jsx
@@ -0,0 +1,44 @@
+import { View, Text, StyleSheet } from 'react-native';
+import { StatusBar } from 'expo-status-bar';
+import Button from '../components/button.component';
+
+const HomeScreen = ({ navigation }) => {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>Home Screen</Text>
+      <Button
+        title='About Us'
+        onPress={() => navigation.navigate('Details')}
+        style={styles.button}
+      />
+      <Button
+        title='Locations'
+        onPress={() => navigation.navigate('Locations')}
+        style={styles.button}
+      />
+      <StatusBar style='auto' />
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#add8e6',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  title: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    marginBottom: 20, // Add some space between the title and the paragraph
+  },
+  button: {
+    backgroundColor: '#00bfff',
+    padding: 10,
+    borderRadius: 15,
+    margin: 10,
+  },
+});
+
+export default HomeScreen;
