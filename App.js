86d3967747db544db8b84e770698ec7a4cd27cc4
binkybarnes <132546955+binkybarnes@users.noreply.github.com>
2024-04-07 09:44:19 -0700

    markers with custom callouts added:

--- App.js
+++ App.js
@@ -1,16 +1,49 @@
+import React, { useRef } from "react";
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
-import MapView, { PROVIDER_GOOGLE } from "react-native-maps";
+import { StyleSheet, Text, View, Button } from "react-native";
+import MapView, { PROVIDER_GOOGLE, Marker, Callout } from "react-native-maps";
+import { markers } from "./assets/markers";
 
 export default function App() {
+  const mapRef = useRef(null);
+
+  const zoomToMarkers = () => {
+    const coordinates = markers.map((marker) => marker.latlng);
+    mapRef.current.fitToCoordinates(coordinates, {
+      edgePadding: { top: 50, right: 50, bottom: 50, left: 50 },
+      animated: true,
+    });
+  };
+
+  // const getRandomHexColor = () => {
+  //   return "#" + Math.floor(Math.random() * 16777215).toString(16); // Random color in hexadecimal format
+  // };
+
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
-      <Text> el gato </Text>
+      <Button onPress={zoomToMarkers} title="rat" color="#841584" />
       <MapView
-        style={{ height: "50%", width: "100%" }}
+        ref={mapRef}
+        initialRegion={{
+          latitude: 32.88118132880787,
+          longitude: -117.23442488535535,
+          latitudeDelta: 0.05,
+          longitudeDelta: 0.05,
+        }}
+        style={{ height: "80%", width: "100%" }}
         provider={PROVIDER_GOOGLE}
-      />
+      >
+        {markers.map((marker, index) => (
+          <Marker key={index} coordinate={marker.latlng}>
+            <Callout tooltip>
+              <View style={styles.calloutdesc}>
+                <Text>{marker.title}</Text>
+              </View>
+            </Callout>
+          </Marker>
+        ))}
+      </MapView>
       <StatusBar style="auto" />
     </View>
   );
@@ -22,5 +55,12 @@ const styles = StyleSheet.create({
     backgroundColor: "#fff",
     alignItems: "center",
     justifyContent: "center",
+  },
+  calloutdesc: {
+    flexDirection: "column",
+    alignSelf: "flex-start",
+    padding: 10,
+    borderRadius: 10,
+    backgroundColor: "#FFFFFF",
   },
 });
